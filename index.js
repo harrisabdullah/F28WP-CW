70df60f794c1b2616b8610ccffccdc5519b684a6
Harris <harris@Harriss-MacBook-Air.local>
2025-10-27 15:43:53 +0000

    added search page

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!')
+  res.sendFile(path.join(__dirname, 'public', 'search', 'index.html'))
 })
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
-
