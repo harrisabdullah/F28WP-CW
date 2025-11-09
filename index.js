25397692d50b4c4515dbaf0de18c07cfce2034be
Harris <harris@Harriss-MacBook-Air.local>
2025-11-09 15:22:47 +0000

    started serving pages

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ const port = 3000
 
 const db = require('./dbinit')
 
+// pages
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'search', 'index.html'))
 })
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login', 'index.html'))
+})
+
+app.get('/sign-up', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'sign-up', 'index.html'))
+})
+
+app.get('/bookings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'bookings', 'index.html'))
+})
+
+app.get('/error', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'error', 'index.html'))
+})
+
+app.get('/hotels/:hotelId', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'hotel-template', 'index.html'))
+})
+
+
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
+
