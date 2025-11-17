037fe9518242d0dbf8232ac21b1bc1f7f682b66b
Harris <harris@Harriss-MacBook-Air.local>
2025-11-17 12:37:47 +0000

    added buildGetBookingsApi

---
+++ api/buildGetBookingsQuery.js
@@ -0,0 +1,15 @@
+const { testSchema, roomConfigSchema } = require('./schema');
+
+const getBookingSchema = {
+    'userID': val => Number.isInteger(val)
+}
+
+function buildGetBookingsApi(body){
+    if (!testSchema(getBookingSchema, body, true)){
+        return -1;
+    }
+    const sql = "SELECT * FROM Bookings WHERE user = ?"
+    return [sql, [body.userID]]
+}
+
+module.exports = buildGetBookingsApi
