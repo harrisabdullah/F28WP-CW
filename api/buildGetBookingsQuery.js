56a1990e6ac58c4f1d857c20c2227e90569828a4
Harris <harris@Harriss-MacBook-Air.local>
2025-11-17 12:50:33 +0000

    added docs

--- api/buildGetBookingsQuery.js
+++ api/buildGetBookingsQuery.js
@@ -4,6 +4,19 @@ const getBookingSchema = {
     'userID': val => Number.isInteger(val)
 }
 
+/**
+ * Generates a SQL query and its parameters for retrieving bookings for a specific user.
+ *
+ * @param {Object} body - JavaScript object containing the user ID.
+ *   Example fields:
+ *     - userID: number (ID of the user whose bookings are being retrieved)
+ *
+ * @returns {Array|number} Returns an array of the form:
+ *   [sqlQueryString, parametersArray]
+ *   - sqlQueryString: string containing SQL with placeholders
+ *   - parametersArray: array of values to substitute in the query
+ * Returns -1 if the input validation fails.
+ */
 function buildGetBookingsApi(body){
     if (!testSchema(getBookingSchema, body, true)){
         return -1;
