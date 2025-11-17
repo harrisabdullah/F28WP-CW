c0d71f4739f42015b6793ed4e5f2ba6020d619ab
Robbie Humphris <rh3011@hw.ac.uk>
2025-11-17 17:42:29 +0000

    ergdtf

--- public/login/script.js
+++ public/login/script.js
@@ -1,15 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-
-document.getElementById("loginForm").addEventListener("submit", function (e) {     
+document.getElementById("login-form").addEventListener("submit", function (e) { 
+    e.preventDefault();    
 
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-       
-        message.textContent = "Login successful!";
+        console.log(username);
+        console.log(password);
 
-        window.location.href = "search.html"; 
-        
-    });
-
+    
+    fetch(window.location.origin + "/api/login", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
 });
+
