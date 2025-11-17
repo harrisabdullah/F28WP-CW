f19382c396dbf7b3887fea779a65c2274f93a509
Joao <jmp4000@hw.ac.uk>
2025-11-17 13:22:59 +0000

    Made login and signup api

--- api/login.js
+++ api/login.js
@@ -1,54 +1,11 @@
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-/**
- * Registers a new user to the database. It records username, email, and password.
- * The passwords are hashed using bycrypt before storing them.
- * @param {Object} req - The request object containing username, email, and password.
- * @param {Obejct} res  - The response object.
- * @returns A message saying whether the registration worked or not.
- */
-exports.signup = (req, res) => {
-    const { username, email, password } = req.body;
+async function checkPass (db, password, dbPassword){
+    return password == dbPassword;
+}
 
-    if(!username || !email || !password) {
-        return res.status(400).json({ message: 'All fields are required.' });
-    }
+function makeToken(user){
+    return jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+}
 
-    const checkUser = 'SELECT * FROM users WHERE email = ?';
-    if(checkUser)
-        return res.status(409).json({ message: 'User already exists.' });
-
-    const hashedPassword = hashPassword(password);
-
-    const insertUser = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-    insertUser.run(email, username, hashedPassword);
-    return res.status(201).json({ message: 'User registered successfully.' });
-};
-
-/**
- * Searches the database for the user and checks if the password is correct. 
- * Then it genereates a JWT token for the user and logs them into the website. 
- * @param {Object} req - The request object containing username and password.
- * @param {Object} res - The response object.
- * @returns A message whether the login worked or not and the JWT token.
- */
-exports.login = (req, res) => {
-    const { username, password } = req.body;
-    if(!username || !password) {
-        return res.status(400).json({ message: 'Username and password are required.' });
-    }
-
-    if(!username) {
-        return res.status(404).json({ message: 'User not found.' });
-    }
-
-    const checkPW = bcrypt.compareSync(password, user.password);
-    if(!checkPW) {
-        return res.status(401).json({ message: 'Invalid password.' });
-    }
-    
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
-
-    return res.status(200).json({ message: 'Login successful.', token });
-};
+module.exports = { checkPass, makeToken };
--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const port = 3000
 
 const { dbinit } = require('./dbUtils')
 const buildHotelSearchQuery = require('./api/buildHotelSearchQuery')
+const loginUtil = require('./api/login')
+const signupUtil = require('./api/signup')
 
 db = dbinit();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // pages
 
@@ -57,4 +60,58 @@ app.post('/api/search', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
+})
+
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+    if(!username || !password) {
+        return res.status(400).json({ message: 'Username and password are required.' });
+    }
+
+    const query = 'SELECT * FROM Users WHERE username = ?'
+
+    db.get(query, [username], async (err, user) => {
+        if(err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Database error. '})
+        }
+
+        if(!user) {
+        return res.status(404).json({ error: 'User not found.' });
+    }
+    const checkPW = loginUtil.checkPass(db, password, user.password);
+        if(!checkPW) {
+            return res.status(401).json({ message: 'Invalid password.' });
+    }
+    const token = loginUtil.makeToken(user);
+    return res.status(200).json({ message: 'Login successful.', token, userID: user.ID });
+    })
+})
+
+app.post('/api/signup', (req, res) => {
+    const { username, password } = req.body;
+
+    if(!username || !password) {
+        return res.status(400).json({ message: 'All fields are required.' });
+    }
+
+    signupUtil.checkUser(db, async (err, user) => {
+        if(err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Database error. '});
+        }
+
+        if(user) {
+        return res.status(409).json({ error: 'User already exists.' });
+        }
+        
+        
+        signupUtil.addUser(db, username, password, function(err) {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database error. '})
+                }  
+        })
+        return res.status(201).json({ message: 'User registered successfully.' , userID: this.lastID});
+    })
 })
